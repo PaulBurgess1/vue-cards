3847ca650d6947213be5f574ed8f64306089c3e6
PaulBurgess <burg0920@mylaurier.ca>
2021-09-14 09:04:53 -0400

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="header-container row gx-0">
+    <div class="header-title">
+        <h1 class="header-title">
+
+            Cards
+            
+        </h1>
+    </div>
+    
+</div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.header-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align:center;
+    background-color: var(--clr-bg-hf);
+    color: var(--clr-text);
+    padding-top:1rem;
+    padding-bottom: 1rem;
+}
+h1{
+    color: var(--clr-text);
+}
+.header-title{
+    white-space: nowrap;
+    color: var(--clr-text);
+}
+
+</style>
---
+++ src/components/HighOrLow.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hol-container">
+    <h1>Higher Or Lower</h1>
+    <div class="new-game">
+        <button class="new-game-btn">Start New Game</button>
+    </div>
+    <div v-if="deck_id" class="game-area">
+      <div class="row">
+        <div class="deck-cnt">
+          {{deck.remaining}}
+        </div>
+        <div class="score">
+          <h3>Score: {{score}}</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="player-card">
+
+        </div>
+        <div class="other-card">
+          
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HighOrLow',
+  data(){
+    return{
+      API_URL: "https://deckofcardsapi.com/api/deck/",
+      deck: "",
+      deck_id: "",
+      score: 0,
+
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h1{
+  color:black;
+}
+</style>
